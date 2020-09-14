d4a79dec7f29589ba72c55c7ae7c9cbe46f33f5b
sinai228 <purplelove228@gmail.com>
2020-09-14 14:43:14 +0900

    done

---
+++ lab01/myproj/App.js
@@ -0,0 +1,25 @@
+/* Name: Sinai Park(sp46)
+CS 262_Lab01
+9 / 4 / 2020 */
+
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello, Expo!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
